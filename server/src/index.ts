f1d2f50380bddc52e0285873d30cfa64d6e257ad
Ombeni Faraja <mwenzangu.ombeni@strathmore.edu>
2025-03-21 00:19:07 +0100

    configured the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,13 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { createMergeableStore } from 'tinybase';
+import { createDurableObjectStoragePersister } from 'tinybase/persisters/persister-durable-object-storage';
+import { getWsServerDurableObjectFetch, WsServerDurableObject } from 'tinybase/synchronizers/synchronizer-ws-server-durable-object';
+
+export class GroceriesDurableObject extends WsServerDurableObject {
+	createPersister() {
+		return createDurableObjectStoragePersister(createMergeableStore(), this.ctx.storage);
+	}
+}
 
 export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+	fetch: getWsServerDurableObjectFetch('GroceriesDurableObjects'),
+};
